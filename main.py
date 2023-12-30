4ef1b01a2b7cf87cf5f63f4085e0430694a006c0
erdnaxeler <55086786+erdnaxeler@users.noreply.github.com>
2023-12-30 15:54:18 -0500

    Centering

--- main.py
+++ main.py
@@ -19,20 +19,23 @@ def display_message(email_rev_share_current):
         st.info("Email Revenue share should be around 30%. You are likely facing either over-attribution of revenue or a lack of performance at the top of your funnel. Reach out to us for a more detailed audit.")
 
 def main():
-    # Inject custom CSS with improved targeting
+    # Inject custom CSS for centering and styling
     st.markdown("""
         <style>
-            div[data-testid="stNumberInput"] > div {
-                width: 200px !important;
+            .css-2trqyj {  /* Main container */
+                align-items: center;
             }
             .stTextInput > div > div > input, .stNumberInput > div > div > input {
                 font-size: 20px !important;
+                width: 200px !important;  /* Width of input fields */
             }
             h1, h2, h3, h4, h5, h6 {
                 text-align: center !important;
                 font-size: 30px !important;
             }
-            div[data-testid="stButton"] > button {
+            .stButton > button {
+                display: block;
+                margin: 10px auto;  /* Center-align buttons */
                 width: 200px !important;
             }
             div[data-testid="stMarkdownContainer"] {
@@ -43,18 +46,18 @@ def main():
 
     st.title("Performance Contract Simulation")
 
-    col1, col2, col3 = st.columns([1, 2, 1])
-    with col2:
-        total_revenue = st.number_input("Total Revenue (USD)", min_value=0.0, format='%f', step=1000.0)
-        email_revenue = st.number_input("Email Revenue (USD)", min_value=0.0, format='%f', step=1000.0)
+    # Centered elements using a single column
+    total_revenue = st.number_input("Total Revenue (USD)", min_value=0.0, format='%f', step=1000.0)
+    email_revenue = st.number_input("Email Revenue (USD)", min_value=0.0, format='%f', step=1000.0)
 
-        if st.button("Calculate"):
-            with st.spinner("Hang on while we compile your data..."):
-                time.sleep(5)  # Simulating data processing time
-                email_rev_share_current, monthly_missed_revenue = calculate_metrics(total_revenue, email_revenue)
-                st.metric(label="Email Revenue Share - Current Level", value=f"{email_rev_share_current:.2%}")
-                if monthly_missed_revenue is not None:
-                    st.metric(label="Email Revenue - Monthly Missed Revenue", value=f"${monthly_missed_revenue:,.2f} USD")
-                display_message(email_rev_share_current)
+    if st.button("Calculate"):
+        with st.spinner("Hang on while we compile your data..."):
+            time.sleep(5)  # Simulating data processing time
+            email_rev_share_current, monthly_missed_revenue = calculate_metrics(total_revenue, email_revenue)
+            st.metric(label="Email Revenue Share - Current Level", value=f"{email_rev_share_current:.2%}")
+            if monthly_missed_revenue is not None:
+                st.metric(label="Email Revenue - Monthly Missed Revenue", value=f"${monthly_missed_revenue:,.2f} USD")
+            display_message(email_rev_share_current)
+
 if __name__ == "__main__":
     main()
